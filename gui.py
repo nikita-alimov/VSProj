dc3e0dc8d0d0335cfbc95569b520aa9275d72e2f
nikita-alimov <kakasilox12@yandex.ru>
2025-05-18 10:32:31 +0300

    Filtration done for the most

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QTextEdit, QPushButton, QLineEdit, QSizePolicy, QShortcut, QLineEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QTextEdit, QPushButton, QLineEdit, QSizePolicy, QShortcut, QLineEdit, QComboBox
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl, QTimer
 from PyQt5.QtGui import QKeySequence, QTextCharFormat, QTextCursor, QColor
@@ -21,14 +21,17 @@ class WebScrapingInterface(QMainWindow):
         layout = QVBoxLayout()
         layout2 = QVBoxLayout()
         layout3 = QHBoxLayout()
+        layout4 = QHBoxLayout()
+        layout5 = QHBoxLayout()
         layoutFinal = QVBoxLayout()
 
         # URL input field
         self.url_input = QLineEdit(self)
         self.url_input.setPlaceholderText("Enter website URL...")
         self.url_input.setText("https://www.google.com")
-                # Connect Enter key press to load_website
+        # Connect Enter key press to load_website
         self.url_input.returnPressed.connect(self.load_website)
+        
 
         layoutFinal.addWidget(self.url_input)
 
@@ -75,12 +78,47 @@ class WebScrapingInterface(QMainWindow):
         self.selenium_scrape_button.clicked.connect(self.selenium_scrape_html)
         layout2.addWidget(self.selenium_scrape_button)
 
+        # Dropdown menus for tags and attributes
+        self.tags_dropdown = QComboBox(self)
+        self.tags_dropdown.setPlaceholderText("Select a tag")
+        layout4.addWidget(self.tags_dropdown)
+
+        self.attributes_dropdown = QComboBox(self)
+        self.attributes_dropdown.setPlaceholderText("Select an attribute")
+        layout5.addWidget(self.attributes_dropdown)
+        self.tags_dropdown.setFixedWidth(self.attributes_dropdown.sizeHint().width())
+
+        # Поля для отображения текущих фильтров
+        self.filtered_tags_view = QLineEdit(self)
+        self.filtered_tags_view.setReadOnly(True)
+        layout4.addWidget(self.filtered_tags_view)
+
+        self.filtered_attributes_view = QLineEdit(self)
+        self.filtered_attributes_view.setReadOnly(True)
+        layout5.addWidget(self.filtered_attributes_view)
+
+        # Button to clear the last entry in the filtered tags view
+        self.clear_tags_button = QPushButton("Удалить последний тег", self)
+        self.clear_tags_button.clicked.connect(self.remove_last_tag)
+        layout4.addWidget(self.clear_tags_button)
+
+        # Button to clear the last entry in the filtered attributes view
+        self.clear_attributes_button = QPushButton("Удалить последний аттрибут", self)
+        self.clear_attributes_button.clicked.connect(self.remove_last_attribute)
+        layout5.addWidget(self.clear_attributes_button)
+        self.clear_tags_button.setFixedWidth(self.clear_attributes_button.sizeHint().width())
+
+
+        # Set layout for the main window
         layout3.addLayout(layout)
         layout3.addLayout(layout2)
         layoutFinal.addLayout(layout3)
+        layoutFinal.addLayout(layout4)
+        layoutFinal.addLayout(layout5)
         container = QWidget()
         container.setLayout(layoutFinal)
         self.setCentralWidget(container)
+
 
         # Html view search functionality
         self.search_results = []  # Список всех найденных вхождений
@@ -91,6 +129,9 @@ class WebScrapingInterface(QMainWindow):
 
         self.prev_result_shortcut = QShortcut(QKeySequence("Shift+F3"), self)
         self.prev_result_shortcut.activated.connect(lambda: self.move_to_search_result(-1))  # Предыдущее вхождение
+
+        self.tags_dropdown.activated.connect(self.filter_by_tag)
+        self.attributes_dropdown.activated.connect(self.filter_by_attribute)
 
         # Подключить сигнал contentsChanged к методу обновления размеров scrollbar_marks
         self.html_view.document().contentsChanged.connect(self.update_scrollbar_marks_size)
@@ -105,6 +146,10 @@ class WebScrapingInterface(QMainWindow):
         self.resize_timer = QTimer(self)
         self.resize_timer.setSingleShot(True)  # Таймер срабатывает только один раз
         self.resize_timer.timeout.connect(self.on_resize_finished)  # Подключить обработчик
+
+        # Initialize dropdowns
+        # self.tags_dropdown.addItem("Loading...")
+        # self.attributes_dropdown.addItem("Loading...")
         
     def resizeEvent(self, event):
         """Обновить размеры scrollbar_marks при изменении размеров окна."""
@@ -115,6 +160,22 @@ class WebScrapingInterface(QMainWindow):
         """Обработчик завершения изменения размера окна."""
         self.update_scrollbar_marks_size()
         self.update_scrollbar_marks()  # Обновить пометки на скроллбаре
+
+    def remove_last_tag(self):
+        """Удалить последний тег из QLineEdit и обновить фильтрацию."""
+        tags = self.filtered_tags_view.text().strip().split(";")
+        if tags:
+            tags.pop()  # Удалить последний тег
+            self.filtered_tags_view.setText(";".join(tags))  # Обновить QLineEdit
+            self.update_filter_from_line_edit()  # Обновить фильтрацию
+
+    def remove_last_attribute(self):
+        """Удалить последний атрибут из QLineEdit и обновить фильтрацию."""
+        attributes = self.filtered_attributes_view.text().strip().split(";")
+        if attributes:
+            attributes.pop()  # Удалить последний атрибут
+            self.filtered_attributes_view.setText(";".join(attributes))  # Обновить QLineEdit
+            self.update_filter_from_line_edit()  # Обновить фильтрацию
 
     def update_url_input(self, url):
         self.url_input.setText(url.toString())
@@ -295,6 +356,10 @@ class WebScrapingInterface(QMainWindow):
         # url = self.url_input.text()
         page = self.web_view.page()
         page.toHtml(lambda html: self.html_view.setPlainText(self.display_pretty_html(html)))
+        page.toHtml(lambda html: self.update_dropdowns(self.display_pretty_html(html)))  # Обновить выпадающие меню
+        self.filtered_tags_view.clear()  # Очистить QLineEdit для тегов
+        self.filtered_attributes_view.clear()  # Очистить QLineEdit для атрибутов
+        # page.toHtml(lambda html: self.update_filter_from_line_edit())  # Обновить фильтрацию
 
         # page.toHtml(lambda html: print(html))
         # page.toHtml(lambda html: self.write_to_file(html))
@@ -319,9 +384,12 @@ class WebScrapingInterface(QMainWindow):
                 response = requests.get(url)
                 response.raise_for_status()  # Проверяем наличие ошибок
                 html = response.text
-                self.html_view.setText(html)  # Устанавливаем HTML в текстовое поле
+                pretty_html = self.display_pretty_html(html)
+                self.html_view.setPlainText(pretty_html)  # Устанавливаем HTML в текстовое поле
+
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching URL: {e}")    
+
     # Selenium Scrape button
     def selenium_scrape_html(self):
         url = self.url_input.text()
@@ -336,11 +404,142 @@ class WebScrapingInterface(QMainWindow):
                 driver.get(url)
                 html_source = driver.page_source
                 pretty_html = self.display_pretty_html(html_source)
-                self.html_view.setText(pretty_html)  # Устанавливаем отформатированный HTML
+                self.html_view.setPlainText(pretty_html)  # Устанавливаем отформатированный HTML
             finally:
                 driver.quit()
 
+    def update_dropdowns(self, html):
+        """Обновить выпадающие меню тегов и атрибутов."""
+        soup = BeautifulSoup(html, 'html.parser')
+        # Извлечь все уникальные теги
+        tags = {tag.name for tag in soup.find_all()}
+        self.tags_dropdown.clear()
+        self.tags_dropdown.addItems(sorted(tags))
 
+        # Извлечь все уникальные атрибуты с их значениями
+        attributes_with_values = set()
+        for tag in soup.find_all():
+            for attr, value in tag.attrs.items():
+                # Добавить атрибуты в формате "атрибут=значение"
+                if isinstance(value, list):  # Если значение атрибута — список (например, class)
+                    attributes_with_values.add(f"{attr}={' '.join(value)}")
+                else:
+                    attributes_with_values.add(f"{attr}={value}")
+
+        self.attributes_dropdown.clear()
+        self.attributes_dropdown.addItems(sorted(attributes_with_values))
+
+    def filter_by_tag(self):
+        """Фильтровать элементы HTML по выбранному тегу."""
+        # Обновить текстовое поле с текущими тегами
+        current_text = self.filtered_tags_view.text()
+        if self.tags_dropdown.currentText() in current_text.split(";"):
+            return
+        
+        soup = BeautifulSoup(self.html_view.toPlainText(), 'html.parser')  # Использовать полученный HTML
+
+        # Получить текущий выбранный тег из выпадающего меню
+        selected_tag = self.tags_dropdown.currentText()
+        if selected_tag:
+            filtered_elements = soup.find_all(selected_tag)
+            soup = BeautifulSoup("\n".join(str(element) for element in filtered_elements), 'html.parser')
+            self.html_view.setPlainText(self.display_pretty_html(str(soup)))
+            if current_text:
+                self.filtered_tags_view.setText(f"{current_text};{selected_tag}")
+            else:
+                self.filtered_tags_view.setText(selected_tag)
+            self.update_dropdowns(self.html_view.toPlainText())  # Обновить выпадающие меню с отфильтрованным HTML
+
+            
+
+    def filter_by_attribute(self):
+        """Фильтровать элементы HTML по выбранному атрибуту."""
+        # Обновить текстовое поле с текущими атрибутами      
+        current_text = self.filtered_attributes_view.text()
+        selected_attribute_value = self.attributes_dropdown.currentText()
+        if selected_attribute_value in current_text.split(";"):
+            return
+        
+        soup = BeautifulSoup(self.html_view.toPlainText(), 'html.parser') 
+
+        # Разделить выбранное значение на атрибут и значение
+        if "=" in selected_attribute_value:
+            attr, value = selected_attribute_value.split("=", 1)
+        else:
+            attr, value = selected_attribute_value, None
+
+        # Фильтровать элементы
+        filtered_elements = []
+        for tag in soup.find_all():
+            if value is not None:  # Если указано значение атрибута
+                attr_value = tag.attrs.get(attr)
+                if isinstance(attr_value, list):  # Если значение атрибута — список (например, class)
+                    if all(val.strip() in attr_value for val in value.split()):
+                        filtered_elements.append(tag)      
+                elif attr_value == value:  # Если значение атрибута — строка
+                    filtered_elements.append(tag)
+            else:  # Если фильтруем только по наличию атрибута
+                if attr in tag.attrs:
+                    filtered_elements.append(tag)
+        soup = BeautifulSoup("\n".join(str(element) for element in filtered_elements), 'html.parser')
+        self.html_view.setPlainText(self.display_pretty_html(str(soup)))
+        self.update_dropdowns(self.html_view.toPlainText())
+        if current_text:
+            self.filtered_attributes_view.setText(f"{current_text};{selected_attribute_value}")
+        else:
+            self.filtered_attributes_view.setText(selected_attribute_value)
+
+
+    def apply_filters(self, html, tags, attributes):
+        """Применить фильтры к HTML-коду."""
+        soup = BeautifulSoup(html, 'html.parser')  # Использовать полученный HTML
+
+        # Фильтровать по тегам
+        if tags:
+            filtered_elements = []
+            for tag in tags:
+                filtered_elements = soup.find_all(tag)
+                soup = BeautifulSoup("\n".join(str(element) for element in filtered_elements), 'html.parser')
+            
+
+        # Фильтровать по атрибутам
+        if attributes:
+            for attribute in attributes:
+                filtered_elements = []
+                if "=" in attribute:
+                    attr, value = attribute.split("=", 1)   
+                else:
+                    attr, value = attribute, None            
+                for tag in soup.find_all():
+                    if value is not None:
+                        attr_value = tag.attrs.get(attr)
+                        if isinstance(attr_value, list):  # Если значение атрибута — список (например, class)
+                            # Проверяем, содержится ли любое слово из value в списке
+                            if all(val.strip() in attr_value for val in value.split()):
+                                filtered_elements.append(tag)
+                                
+                        elif attr_value == value:  # Если значение атрибута — строка
+                            # Проверяем, совпадает ли строка или содержит искомое значение
+                                filtered_elements.append(tag) 
+                    else:  # Если фильтруем только по наличию атрибута
+                        if attr in tag.attrs:
+                            filtered_elements.append(tag)                                    
+                soup = BeautifulSoup("\n".join(str(element) for element in filtered_elements), 'html.parser')
+
+        # Обновить отображение в html_view
+        self.html_view.setPlainText(self.display_pretty_html(str(soup)))
+        self.update_dropdowns(self.html_view.toPlainText())  # Обновить выпадающие меню с отфильтрованным HTML
+
+    def update_filter_from_line_edit(self):
+        """Обновить фильтрацию на основе содержимого QLineEdit."""
+        # Получить текущие теги и атрибуты из QLineEdit
+        tags = [tag.strip() for tag in self.filtered_tags_view.text().strip().split(";") if tag.strip()]
+        attributes = [attr.strip() for attr in self.filtered_attributes_view.text().strip().split(";") if attr.strip()]
+
+        # Использовать исходный HTML
+        page = self.web_view.page()
+        page.toHtml(lambda html: self.apply_filters(html, tags, attributes))  # Передать HTML в обработчик
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
